716b22b8e1dc7b6ff562a852ec675239bc4fc9d0
Luke Massa <lmassa@tripadvisor.com>
2021-02-11 22:19:08 -0500

    First pass

---
+++ numbers.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+import argparse
+import unittest
+
+DEFAULT_MAX = 100
+
+SMALL = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+TENS = [None, None, "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+
+def number_to_word(number):
+    if number < len(SMALL):
+        return SMALL[number]
+    digits = list(reversed([int(x) for x in str(number)]))
+    if number < 100:
+        if digits[0] == 0:
+            return TENS[digits[1]]
+        return "%s %s" % (TENS[digits[1]], SMALL[digits[0]])
+    if number < 1000:
+        two_digits = 10 * digits[1] + digits[0]
+        hundred = SMALL[digits[2]] + " hundred"
+        if two_digits == 0:
+            return hundred
+        return "%s %s" % (hundred, number_to_word(two_digits))
+
+
+    raise ValueError("Unexpected number %s" % (number,))
+
+def run_tests():
+    """
+    Tests
+    """
+
+    class Tester(unittest.TestCase):
+
+        def one_test(self, number, expected_word):
+            actual_word = number_to_word(number)
+            self.assertEqual(expected_word, actual_word)
+
+        def test_small(self):
+            self.one_test(0, "zero")
+            self.one_test(2, "two")
+            self.one_test(7, "seven")
+            self.one_test(9, "nine")
+            self.one_test(15, "fifteen")
+
+        def test_tens(self):
+            self.one_test(20, "twenty")
+            self.one_test(21, "twenty one")
+            self.one_test(35, "thirty five")
+            self.one_test(99, "ninety nine")
+
+        def test_hundreds(self):
+            self.one_test(100, "one hundred")
+            self.one_test(500, "five hundred")
+            self.one_test(520, "five hundred twenty")
+            self.one_test(329, "three hundred twenty nine")
+
+
+    unittest.TextTestRunner(verbosity=2).run(unittest.TestLoader().loadTestsFromTestCase(Tester))
+
+def show(max_number):
+    numbers = [number_to_word(x) for x in range(1, max_number+1)]
+    for number in sorted(numbers):
+        print(number)
+
+
+def main():
+
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("action", choices=["test", "show"])
+    parser.add_argument("--max-number", type=int)
+    args = parser.parse_args()
+
+    max_number = args.max_number
+    if max_number is None:
+        max_number = DEFAULT_MAX
+    
+    if args.action == "test":
+        run_tests()
+    elif args.action == "show":
+        show(max_number)
+    else:
+        raise ValueError("Unexpected action %s" % (args.action))
+
+if __name__ == "__main__":
+    main()
